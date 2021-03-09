b0020dfea9d1d86f751f5a7bf1622144c97ed497
Glitch (hello-webpage) <none>
2021-03-09 18:00:03 +0000

    🐋🐜 Checkpoint ./script.js:8331381/1553

--- script.js
+++ script.js
@@ -1,51 +1,44 @@
-//import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
 import { Scene, PerspectiveCamera, WebGLRenderer } from 'https://unpkg.com/three@0.126.1/build/three.module.js'
 import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls";
 import {createSculpture, createSculptureWithGeometry} from '/shader-park-core.esm.js';
 
 let scene = new Scene();
+let params = { time: 0 };
 
 let camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 30;
+camera.position.z = 5;
 
 let renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio( window.devicePixelRatio );
 document.body.appendChild( renderer.domElement );
 
-
 // Shader Park setup
 let spCode = `
-let strength = input(0.6, 0, 3);
-let twistMix = input();
-setStepSize(.3);
+let duration = time / 30 * TWO_PI;
+let animationLoop = abs(sin(duration));
+let oscillation = clamp(abs(sin(duration)) - .6, 0.0, 1.0);
+setStepSize(.13);
+rotateX(animationLoop);
+metal(0.2)
+shine(.1)
+let glo = max(1.3-2.0*dot(-1.0*normal,getRayDirection()),1.);
+let s = getSpace();
+let n = noise(s*5.2*sin(duration)+cos(duration));
+n = sin(n+1000000);
+let col = 1-pow(n, .4);
+let grain = noise(s*100000)*.3;
+color(vec3(0,1, 5)*col);
 
-rotateY(time * .2);
-let warpedSpace = warpSpace(getSpace());
-metal(.9);
-shine(1);
-color(1 - warpedSpace);
-sphere(.2 + length(warpedSpace) * .2);
-
-// inspired by https://www.shadertoy.com/view/ttlGDf
-function warpSpace(p) {
-  let t = time / 4.;
-  rotateY(getRayDirection().y * (1 - twistMix) * 12);
-  p = getSpace().x*8.0*(vec3(0.5, .2, .1) + p);
-  for(let i = 1.0; i < 3.0; i += 1.0) { 
-      p.x = p.x +strength * sin(2.0 * t + i * 1.5 * p.y) + t * 0.5;
-      p.y = p.x + strength * cos(2.0 * t + i * 1.5 * p.x);
-  }
-  return  0.5 + 0.5 * cos(time + vec3(p.x, p.y, p.x) + vec3(0., 2., 4.));
-}
+sphere(0.31);
+expand(abs(n)*0.15+(grain*.02*col));
 `
 
 let mesh = createSculpture(spCode, () => ( {
-  
+  time: params.time
 } ));
 
 scene.add(mesh);
-
 
 let controls = new OrbitControls( camera, renderer.domElement, {
   enableDamping : true,
@@ -56,6 +49,7 @@ let controls = new OrbitControls( camera, renderer.domElement, {
 
 let render = () => {
   requestAnimationFrame( render );
+  params.time += 0.1;
   controls.update();
   renderer.render( scene, camera );
 };
