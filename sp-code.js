8025f6424b64eac15d28237098471a9713f4a12a
Glitch (hello-webpage) <none>
2021-03-10 18:13:37 +0000

    🏯👱 Checkpoint ./sp-code.js:8331381/123 ./index.html:8331381/148

--- sp-code.js
+++ sp-code.js
@@ -1,20 +1,21 @@
 /* eslint-disable */
 
 export function spCode() {
-    sphere(5);
+  
+  
   let size = input(12, 10, 50.0);
   let gyroidSteps = input(.06, 0, .1)
-//   let s = getSpace();
+  let s = getSpace();
 
-//   let col = vec3(1, 1, 1.5) + normal * .2;
-//   metal(.2);
-//   shine(.5)
-//   col -= length(s);
-//   color(col);
+  let col = vec3(1, 1, 1.5) + normal * .2;
+  metal(.2);
+  shine(.5)
+  col -= length(s);
+  color(col);
 
-//   s += time *.1;
-//   let sdf = min(gyroidSteps, sin(s.x * size) + sin(s.y * size) + sin(s.z * size));
-//   setSDF(sdf)
-//   intersect();
-//   sphere(.5);  
+  s += time *.1;
+  let sdf = min(gyroidSteps, sin(s.x * size) + sin(s.y * size) + sin(s.z * size));
+  setSDF(sdf)
+  intersect();
+  sphere(1);  
 }
