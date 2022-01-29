78ca0199cdb01598d92a60ffcc4e79221e02be26
Glitch (hello-webpage) <none>
2022-01-29 16:43:36 +0000

    🌅🏹 Checkpoint ./script.js:8331381/1848 ./sp-code.js:8331381/402

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-import { Scene, PerspectiveCamera, WebGLRenderer, Color, FontLoader, TextBufferGeometry } from 'https://unpkg.com/three@0.126.1/build/three.module.js'
+
+import { Scene, SphereGeometry, Vector3, PerspectiveCamera, WebGLRenderer, Color, FontLoader, TextBufferGeometry } from 'https://unpkg.com/three@0.126.1/build/three.module.js'
 import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls";
-import { createSculpture, createSculptureWithGeometry } from 'https://unpkg.com/shader-park-core@0.0.20/dist/shader-park-core.esm.js';
+import { createSculpture, createSculptureWithGeometry } from 'https://unpkg.com/shader-park-core@0.1.4/dist/shader-park-core.esm.js';
 import { spCode } from '/sp-code.js';
 
 let scene = new Scene();
 let params = { time: 0 };
 
 let camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+camera.position.z = 3;
 
 let renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -15,31 +16,35 @@ renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setClearColor( new Color(1, 1, 1), 1);
 document.body.appendChild( renderer.domElement );
 
-const loader = new FontLoader();
+let geometry  = new SphereGeometry(2, 45, 45);
 
-loader.load( 'https://cdn.glitch.com/44b034f5-6c9a-414c-96b3-8280ecf82f27%2Fhelvetiker_regular.typeface.json?v=1615399030749', function ( font ) {
-  let geometry = new TextBufferGeometry( 'Hi', {
-    font: font,
-    size: 4,
-    height: .1,
-    curveSegments: 12,
-    bevelEnabled: true,
-    bevelThickness: .01,
-    bevelSize: .1,
-    bevelOffset: 0,
-    bevelSegments: 1
-  } );
-  geometry.computeBoundingSphere();
-  geometry.center();
-  
-  let mesh = createSculptureWithGeometry(geometry, spCode, () => ( {
-    time: params.time,
-    size: 20,
-    gyroidSteps: .01
-  } ));
-  
-  scene.add(mesh);
-} );
+let currMouse = new Vector3();
+let mouse = new Vector3();
+let pointerDown = 0.0;
+
+window.addEventListener( 'pointermove', (event) => {
+  currMouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	currMouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}, false );
+
+window.addEventListener( 'pointerdown', (event) => {
+  pointerDown = 1.0;
+}, false );
+
+window.addEventListener( 'pointerup', (event) => {
+  pointerDown = 0.0;
+}, false );
+
+let mesh = createSculptureWithGeometry(geometry, spCode, () => ( {
+  time: params.time,
+  size: 20,
+  gyroidSteps: .01,
+  pointerDown,
+  mouse
+} ));
+
+scene.add(mesh);
+
 
 let controls = new OrbitControls( camera, renderer.domElement, {
   enableDamping : true,
@@ -60,6 +65,7 @@ let render = () => {
   requestAnimationFrame( render );
   params.time += 0.01;
   controls.update();
+  mouse.lerp(currMouse, .05 );
   renderer.render( scene, camera );
 };
 
--- sp-code.js
+++ sp-code.js
@@ -5,18 +5,11 @@ export function spCode() {
   
   let size = input(12, 10, 50.0);
   let gyroidSteps = input(.06, 0, .1)
+  let pointerDown = input();
   let s = getSpace();
-  displace(getRayDirection());
-  let col = vec3(1, 1, 1.5) + normal * .2;
-  metal(.2);
-  shine(.5)
+  displace(mouse.x, mouse.y, 0);
   
-  col -= length(s)*.7;
-  color(col);
-
-  //s = vec3(s.x, s.y, s.z-time);
-  let sdf = min(gyroidSteps, sin(s.x * size) + sin(s.y * size) + sin(s.z * size));
-  setSDF(sdf)
-  intersect();
-  sphere(1);  
+  sphere(.5);
+  mixGeo(pointerDown);
+  box(vec3(.5));
 }
