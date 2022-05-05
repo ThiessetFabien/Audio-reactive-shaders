e6d1a56e74799d9519b690f8b53758d658969cab
Glitch (hello-webpage) <none>
2022-05-05 15:32:15 +0000

    👬📙 Checkpoint ./sp-code.js:8331381/171

--- sp-code.js
+++ sp-code.js
@@ -5,8 +5,9 @@ export function spCode() {
     metal(.5);
     shine(.5);
     setMaxIterations(5);
-    let n = noise(getSpace() + vec3(0, 0, audio) + noise(getRayDirection()*4 +vec3(0, audio, vec3(0, audio, audio))*.5 ));
-    color(normal * .1 + vec3(0, 0, 1));
+    let n = noise(getSpace() + vec3(0, 0, audio+time*.1) + noise(getRayDirection()*4 +vec3(0, audio, vec3(0, audio, audio))*.5 ));
+    let col = normal * .1 + vec3(0, 0, 1);
+    color(col);
     displace(mouse.x*1.5, mouse.y*1.5, 0);
     boxFrame(vec3(2), abs(n) * .1+.04 );
     mixGeo(pointerDown)
