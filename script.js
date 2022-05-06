4d7670f51eecccf3916270d677345e7d85aa45f0
Glitch (hello-webpage) <none>
2022-05-06 05:54:51 +0000

    👓📚 Checkpoint ./script.js:8331381/7605 ./main.js:8331381/5872 ./script1.js:8331381/158 ./index.html:8331381/3

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 import { AudioListener, Audio, AudioLoader, AudioAnalyser } from 'three';
-import { Scene, SphereGeometry, Vector3, PerspectiveCamera, WebGLRenderer, Color, MeshBasicMaterial, MeshStandardMaterial, Mesh } from 'three';
-import { OrbitControls } from 'https://unpkg.com/three@0.140/examples/jsm/controls/OrbitControls.js';
-import { createSculpture, createSculptureWithGeometry } from 'https://unpkg.com/shader-park-core/dist/shader-park-core.esm.js';
-import { spCode } from '/sp-code.js';
+import { Scene, SphereGeometry, Vector3, PerspectiveCamera, WebGLRenderer, Color, MeshBasicMaterial, Mesh } from 'three';
 
 let scene = new Scene();
 let params = { time: 0 };
@@ -16,77 +13,13 @@ renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setClearColor( new Color(1, 1, 1), 0);
 document.body.appendChild( renderer.domElement );
 
-// AUDIO
-
-// create an AudioListener and add it to the camera
-const listener = new AudioListener();
-camera.add( listener );
-
-// create an Audio source
-const sound = new Audio( listener );
-
-let button = document.querySelector('.button');
-button.innerHTML = "Loading Audio..."
-
-// load a sound and set it as the Audio object's buffer
-const audioLoader = new AudioLoader();
-audioLoader.load( 'https://cdn.glitch.global/59b80ec2-4e5b-4b54-b910-f3441cac0fd6/1%20or%20won%20Beat.m4a?v=1650562076005', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop(true);
-	sound.setVolume(0.5);
-  button.innerHTML = "Play Audio"
-  button.addEventListener('pointerdown', () => {
-    sound.play();
-    button.style.display = 'none';
-  }, false);
-});
-
-
-// create an AudioAnalyser, passing in the sound and desired fftSize
-// get the average frequency of the sound
-const analyser = new AudioAnalyser( sound, 32 );
-
-
-let state = {
-  mouse : new Vector3(),
-  currMouse : new Vector3(),
-  pointerDown: 0.0,
-  currPointerDown: 0.0,
-  audio: 0.0
-}
-
-window.addEventListener( 'pointermove', (event) => {
-  state.currMouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	state.currMouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-}, false );
-
-window.addEventListener( 'pointerdown', (event) => state.currPointerDown = 1.0, false );
-window.addEventListener( 'pointerup', (event) => state.currPointerDown = 0.0, false );
-
 
 let geometry  = new SphereGeometry(2, 45, 45);
-// let material = new MeshBasicMaterial( { color: 0x33aaee } );
+let material = new MeshBasicMaterial( { color: 0x33aaee } );
 
-// // // Create Shader Park Sculpture
-let mesh = createSculptureWithGeometry(geometry, spCode(), () => ( {
-  time: params.time,
-  pointerDown: state.pointerDown,
-  audio: state.audio,
-  mouse: state.mouse,
-  _scale : .5
-} ));
-
-// let mesh = new Mesh(geometry, material);
+let mesh = new Mesh(geometry, material);
 
 scene.add(mesh);
-
-// Add Controlls
-let controls = new OrbitControls( camera, renderer.domElement, {
-  enableDamping : true,
-  dampingFactor : 0.25,
-  zoomSpeed : 0.5,
-  rotateSpeed : 0.5
-} );
 
 let onWindowResize = () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -98,13 +31,6 @@ window.addEventListener( 'resize', onWindowResize );
 
 let render = () => {
   requestAnimationFrame( render );
-  params.time += 0.01;
-  controls.update();
-  if(analyser) {
-    state.audio += Math.pow((analyser.getFrequencyData()[2]/255)*.85, 8)
-  }
-  state.pointerDown = .1 * state.currPointerDown + .9 * state.pointerDown;
-  state.mouse.lerp(state.currMouse, .05 );
   renderer.render( scene, camera );
 };
 
