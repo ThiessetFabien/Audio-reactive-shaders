ddeae4e35162e90631f11740846a628d19739a72
Glitch (hello-webpage) <none>
2022-05-06 06:06:54 +0000

    🏤❣️ Checkpoint ./script.js:8331381/2764

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-import { AudioListener, Audio, AudioLoader, AudioAnalyser } from 'three';
-import { Scene, SphereGeometry, Vector3, PerspectiveCamera, WebGLRenderer, Color, MeshBasicMaterial, Mesh } from 'three';
+import { Scene, SphereGeometry, PerspectiveCamera, WebGLRenderer, Color, MeshBasicMaterial, Mesh } from 'three';
+import { OrbitControls } from 'https://unpkg.com/three@0.140/examples/jsm/controls/OrbitControls.js';
 
 let scene = new Scene();
-let params = { time: 0 };
 
 let camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 camera.position.z = 1.5;
@@ -16,7 +15,6 @@ document.body.appendChild( renderer.domElement );
 
 let geometry  = new SphereGeometry(2, 45, 45);
 let material = new MeshBasicMaterial( { color: 0x33aaee } );
-
 let mesh = new Mesh(geometry, material);
 
 scene.add(mesh);
