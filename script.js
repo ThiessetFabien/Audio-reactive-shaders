cc3006b9cd3866aa33e628ec7a616a307a2dd2e8
Glitch (hello-webpage) <none>
2022-11-16 05:49:32 +0000

    🚪🏋 Checkpoint ./script1.js:8331381/160 ./index.html:8331381/1 ./script.js:8331381/11

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-import { Scene, SphereGeometry, PerspectiveCamera, WebGLRenderer, Color, MeshBasicMaterial, Mesh } from 'three';
+import { Scene, SphereGeometry, Vector3, PerspectiveCamera, WebGLRenderer, Color, MeshBasicMaterial, Mesh } from 'three';
 import { OrbitControls } from 'https://unpkg.com/three@0.140/examples/jsm/controls/OrbitControls.js';
 import { createSculptureWithGeometry } from 'https://unpkg.com/shader-park-core/dist/shader-park-core.esm.js';
+import { spCode } from '/sp-code.js';
 
 let scene = new Scene();
 
 let camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 1.5;
+camera.position.z = 5.5;
 
 let renderer = new WebGLRenderer({ antialias: true, transparent: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -13,14 +14,14 @@ renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setClearColor( new Color(1, 1, 1), 0);
 document.body.appendChild( renderer.domElement );
 
+let button = document.querySelector('.button');
+button.innerHTML = "Loading Audio..."
+button.style.display = 'none';
 
 let geometry  = new SphereGeometry(2, 45, 45);
-// let material = new MeshBasicMaterial( { color: 0x33aaee } );
-// let mesh = new Mesh(geometry, material);
-let mesh = createSculptureWithGeometry(geometry, 'sphere(.7)', () => ( {
-  time: 0
-} ));
+let material = new MeshBasicMaterial( { color: 0x33aaee} );
 
+let mesh = new Mesh(geometry, material);
 
 scene.add(mesh);
 
@@ -42,6 +43,7 @@ window.addEventListener( 'resize', onWindowResize );
 
 let render = () => {
   requestAnimationFrame( render );
+  controls.update();
   renderer.render( scene, camera );
 };
 
